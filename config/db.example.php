ec640c4ad2c81c102dd7790eda970d9f0fb02da8
ArthurZagidullin <design.mgn@gmail.com>
2016-04-03 00:18:11 +0300

    db config example

---
+++ config/db.example.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=citizenhelp',
+    'username' => 'db_name',
+    'password' => 'db_pass',
+    'charset' => 'utf8',
+];
