838e25d6976e7dd33e9f8734c6a9675cc5180cbc
ArthurZagidullin <design.mgn@gmail.com>
2016-04-03 00:13:46 +0300

    create migrates, tables: users, stories, comments; change default view

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '',
+    'password' => 'q1w2e3q1w2e3',
     'charset' => 'utf8',
 ];
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,10 +27,10 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'Гражданская помощь',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'navbar-inverse ',
         ],
     ]);
     echo Nav::widget([
