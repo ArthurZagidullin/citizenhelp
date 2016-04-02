c2d7d7c03e5bddd0cf1abd60905169e9480999d1
ArthurZagidullin <design.mgn@gmail.com>
2016-04-03 00:16:17 +0300

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => 'q1w2e3q1w2e3',
-    'charset' => 'utf8',
-];
